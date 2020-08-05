5eea8c3974bc491e7c77df084fef236d29903548
Matias Nicoletti <matiasnicolettmdp@gmail.com>
2020-08-05 16:20:18 -0300

    Burger controls added

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,18 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)//take the keys of an object and create an array
             .map(el => {
                 return [...Array(props.ingredients[el])].map((_, i)=>{
                     return <BurgerIngredient key={el + i} type={el}  />
                 });
-            }); //take the keys of an object and create an array
+            }).reduce((previous, current)=>{
+                return previous.concat(current);
+            }, []); 
+            console.log(transformedIngredients);
+            if(transformedIngredients.length === 0){
+                transformedIngredients = <p>Please start adding ingredients</p>
+            }
     
             return (
         <div className={classes.Burger}>
