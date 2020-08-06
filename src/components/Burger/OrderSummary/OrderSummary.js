f190c8a8327a3796af9f4444f8e7c3cca187b6c2
Matias Nicoletti <matiasnicolettmdp@gmail.com>
2020-08-06 19:09:39 -0300

    Sidedrawer and project structre changes

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,29 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Auxiliary from '../../../hoc/Auxiliary';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props)=>{
-    const ingredientSummary = Object.keys(props.ingredients)
-            .map(igKey =>{
-            return <li key={igKey}>
-                <span style={{textTransform:'capitalize'}}>
-                    {igKey}
-                </span>
-                : {props.ingredients[igKey]}
+class OrderSummary extends Component {
+    componentWillUpdate(){
+        console.log('order');
+    }
+
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients)
+            .map(igKey => {
+                return <li key={igKey}>
+                    <span style={{ textTransform: 'capitalize' }}>
+                        {igKey}
+                    </span>
+                : {this.props.ingredients[igKey]}
                 </li>
             });
-    return(
-        <Auxiliary>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients: </p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-    <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-            <p>Continue to checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCanceled}>Cancel</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>Continue</Button>
+
+        return (
+            <Auxiliary>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients: </p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to checkout?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCanceled}>Cancel</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued}>Continue</Button>
             </Auxiliary>
-    )
+        );
+    }
+
 };
-export default orderSummary;
+export default OrderSummary;
