83773a143352e0d767bded9b6714e6d32c2912b3
Matias Nicoletti <matiasnicolettmdp@gmail.com>
2020-08-06 23:42:36 -0300

    Interceptors cleaning

--- src/hoc/WithErrorHandler/withErrorHandler.js
+++ src/hoc/WithErrorHandler/withErrorHandler.js
@@ -8,13 +8,19 @@ const withErrorHandler = (WrappedComponent, axios) => {
             error: null
         }
         componentWillMount() {
-            axios.interceptors.request.use(req => {
+            this.requestInterceptor = axios.interceptors.request.use(req => {
                 this.setState({ error: null });
                 return req;
             });
-            axios.interceptors.response.use(res => res, error => {
+            this.responseInterceptor =axios.interceptors.response.use(res => res, error => {
                 this.setState({ error });
             });
+        }
+
+        componentWillUnmount(){
+            console.log('Will unmounbt', this.requestInterceptor, this.responseInterceptor);
+            axios.interceptors.request.eject(this.requestInterceptor);
+            axios.interceptors.response.eject(this.responseInterceptor);
         }
 
         errorConfirmedHandler = ()=>{
